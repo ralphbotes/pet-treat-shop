d03e8b8f00a0fbb13bb00139bdcbde3d08fca4b8
Ralph <ralphbotes@gmail.com>
2024-08-28 23:43:32 +0200

    Mod: implemented calculate

--- treat_shop/TheTreatShop.py
+++ treat_shop/TheTreatShop.py
@@ -1,3 +1,5 @@
+from treat_shop.constants import BUCKET_FILL
+from treat_shop.utils import determine_price, determine_discount_for_all
 
 class TheTreatShop:
     def __init__(self,biscuits,chewies,vitamins,bucket_size,is_weekend):
@@ -8,7 +10,26 @@ class TheTreatShop:
         self.is_weekend = is_weekend
 
     def calculate(self):
-        print("PRICE")
+        total = 0.00
+
+        # Step 1: Calculate price per treat item
+        if self.biscuits > 0:
+            total += determine_price("biscuits", self.biscuits, self.bucket_size,self.is_weekend)
+        
+        if self.chewies > 0:
+            total += determine_price("chewies", self.chewies, self.bucket_size,self.is_weekend)
+            
+        if self.vitamins > 0:
+            total += determine_price("vitamins", self.vitamins, self.bucket_size,self.is_weekend)
+
+        # Step 2: Determine discount for all treats
+        total_treats_count = self.biscuits + self.chewies + self.vitamins
+        total = determine_discount_for_all(total,total_treats_count,self.vitamins,self.chewies,self.bucket_size)
+        
+        # Step 3: Add initial bucket fill amount
+        total += BUCKET_FILL
+        
+        return total
 
 def run():
     biscuits = int(input("Number of biscuits: "))
